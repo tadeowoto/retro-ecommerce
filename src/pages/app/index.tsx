6854ca3bf6dafa66f1d1ef73d05a30703f519bab
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-01-27 11:37:52 -0300

    react-routes added

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,8 +1,41 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "../home";
+import MyAccount from "../myAccount";
+import MyOrders from "../myOrders";
+import NotFound from "../notFound";
+import SignIn from "../signIn";
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+  return routes;
+};
+
 function App() {
   return (
-    <>
-      <h1 className="font-bold font-nunito ">retro react</h1>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
